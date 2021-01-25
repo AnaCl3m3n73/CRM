08afaeeffec46eb5fbbf6ba52d84ca151b16b80b
a.carolina.clemente <a.carolina.clemente@WIN-274L9CMFBS>
2021-01-24 22:09:35 -0300

    Add project files.

---
+++ CRM/Conexao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk.Client;
+
+namespace CRM
+{
+    class Conexao
+    {
+
+    }
+}
---
+++ CRM/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Discovery;
+
+namespace CRM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conexao conexao = new Conexao();
+            //var serviceProxy = conexao.obter();
+            Descoberta();
+            Console.ReadKey();
+        }
+
+
+        #region Descoberta
+        static void Descoberta()
+        {
+
+            Uri local = new Uri("https://disco.crm2.dynamics.com/XRMServices/2011/Discovery.svc");
+
+            ClientCredentials clientcred = new ClientCredentials();
+            clientcred.UserName.UserName = "crmonline2021@treinamentocrmonline.onmicrosoft.com";
+            clientcred.UserName.Password = "Londres2021";
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            DiscoveryServiceProxy dsp = new DiscoveryServiceProxy(local, null, clientcred, null);
+            dsp.Authenticate();
+
+            RetrieveOrganizationsRequest rosreq = new RetrieveOrganizationsRequest();
+            rosreq.AccessType = EndpointAccessType.Default;
+            rosreq.Release = OrganizationRelease.Current;
+            RetrieveOrganizationsResponse r = (RetrieveOrganizationsResponse)dsp.Execute(rosreq);
+
+            foreach (var item in r.Details)
+            {
+                Console.WriteLine("Nome: " + item.UniqueName);
+                Console.WriteLine("Nome Exibição: " + item.FriendlyName);
+                foreach (var endpoint in item.Endpoints)
+                {
+                    Console.WriteLine(endpoint.Key);
+                    Console.WriteLine(endpoint.Value);
+                }
+            }
+            Console.ReadKey();
+
+        }
+        #endregion
+    }
+
+}
